1e539859a83a5b2eaa906283c30dbcd4e60443bd
MariiaRa <35453994+MariiaRa@users.noreply.github.com>
2018-01-22 15:28:29 +0200

    Update HammingTest2.scala

--- src/main/scala/exercism/HammingTest2.scala
+++ src/main/scala/exercism/HammingTest2.scala
@@ -8,12 +8,8 @@ object HammingTest2 extends App{
      if (a.length == 0) 0
      else a.zip(b).filter(pairs => pairs._1 != pairs._2).length
     }
-
-
   }
 
   val compareDNA = new Hamming
   println(compareDNA.distance("GGACGGATTCTG", "AGGACGGATTCT"))
-
-
 }
