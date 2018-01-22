708851d1813b376ecc1f9ed95599425f3bb33ee3
MariiaRa <35453994+MariiaRa@users.noreply.github.com>
2018-01-22 15:16:10 +0200

    Update FlattenArrayTest.scala

--- src/main/scala/exercism/FlattenArrayTest.scala
+++ src/main/scala/exercism/FlattenArrayTest.scala
@@ -6,7 +6,6 @@ object FlattenArrayTest extends App{
 
     def flatten(list: List[Any]): List[Int] = list.filter(_ != null).flatMap{
       case c: List[_] => flatten(c)
-      case b: List[Int] => b
       case a: Int => List(a)
       }
     
