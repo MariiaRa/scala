1e529c5745b3f8e2bcdd9debe732625b5aba7880
MariiaRa <35453994+MariiaRa@users.noreply.github.com>
2018-01-22 16:47:23 +0200

    Update HammingTest2.scala

--- src/main/scala/exercism/HammingTest2.scala
+++ src/main/scala/exercism/HammingTest2.scala
@@ -4,11 +4,11 @@ object HammingTest2 extends App{
 
   class Hamming {
 
-    def distance (a: String, b:String) = {
-     if (a.length == 0) 0
-    /* else a.zip(b).filter(pairs => pairs._1 != pairs._2).length*/
-      else a.zip(b).count(pairs => pairs._1 != pairs._2)
-    }
+    def distance  (a: String, b:String) =
+      if (a.length == b.length) {
+        val dist = a.zip(b).count(pairs => pairs._1 != pairs._2)
+        Some(dist)
+      } else None
   }
 
   val compareDNA = new Hamming
