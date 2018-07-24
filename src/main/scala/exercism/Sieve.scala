df93bfa1037ecaee78c9d41affae64ae11b110be
Maria Rabyk <mariia.rabyk@gmail.com>
2018-07-24 12:56:09 +0300

    Initial commit

---
+++ src/main/scala/exercism/Sieve.scala
@@ -0,0 +1,18 @@
+package exercism
+
+object Sieve {
+
+  def findPrimes(limit: Int) = {
+    def primes(list: List[Int]): List[Int] = list.headOption match {
+      case None => list
+      case Some(head) => head :: primes(list.tail.filter(_ % head != 0))
+    }
+
+    primes((2 to limit).toList)
+  }
+
+  def main(args: Array[String]): Unit = {
+    print(findPrimes(10))
+  }
+
+}
