0e91f59831798ad30f3ee82951fb7ccdef27eb78
Maria Rabyk <mariia.rabyk@gmail.com>
2018-07-24 10:58:44 +0300

    Initial commit

--- src/main/scala/exercism/Triangle.scala
+++ src/main/scala/exercism/Triangle.scala
@@ -1,22 +1,33 @@
 package exercism
 
-case class Triangle(a: Int, b: Int, c: Int)
+object TriangleType extends Enumeration {
+  val Equilateral, Isosceles, Scalene, Illogical = Value
+}
+
+case class Triangle(a: Int, b: Int, c: Int) {
+
+  lazy val triangleType = {
+    if (!checkShape) TriangleType.Illogical
+    else numberOfEqualSides match {
+      case 3 => TriangleType.Equilateral
+      case 2 => TriangleType.Isosceles
+      case _ => TriangleType.Scalene
+    }
+  }
+
+  private def checkShape: Boolean = {
+    if (a <= 0 || b <= 0 || c <= 0) false
+    else if (a + b >= c || a + c >= b || b + c >= a) true
+    else false
+  }
+
+  private def numberOfEqualSides = Set(a, b, c).size
+}
 
 object Triangle {
 
-  def checkTypeOfTriange(in: Triangle): String = {
-    in.productIterator.toList.distinct.length match {
-      case 1 => "Equilateral"
-      case 2 => "Isosceles"
-      case 3 => "Scalene"
-    }
- }
-
   def main(args: Array[String]): Unit = {
-    val shape = Triangle(6, 3, 6)
-
-    println(checkTypeOfTriange(shape))
-
+    val shape = Triangle(10, 4, 6)
+    println(shape.triangleType)
   }
-
 }
